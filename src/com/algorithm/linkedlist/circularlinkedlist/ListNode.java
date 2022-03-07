5ce6d01f19122857015a929e644f2569c1ddda00
Alice <1428235589@qq.com>
2022-03-07 10:29:10 +0800

    环形链表

---
+++ src/com/algorithm/linkedlist/circularlinkedlist/ListNode.java
@@ -0,0 +1,17 @@
+package com.algorithm.linkedlist.circularlinkedlist;
+
+/**
+ * @Version: 1.0
+ * @Author: alice
+ * @Date: 2022-03-07 09:38
+ * @Description:
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}

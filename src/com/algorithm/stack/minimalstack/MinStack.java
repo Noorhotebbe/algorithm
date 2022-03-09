57fdc24a4dd9eef652d1a26fb2708fe79858d93b
Alice <1428235589@qq.com>
2022-03-09 10:44:16 +0800

    最小栈

---
+++ src/com/algorithm/stack/minimalstack/MinStack.java
@@ -0,0 +1,49 @@
+package com.algorithm.stack.minimalstack;
+
+import java.util.Stack;
+
+/**
+ * @Version: 1.0
+ * @Author: alice
+ * @Date: 2022-03-09 09:50
+ * @Description: 最小栈：
+ * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ * <p>
+ * 实现 MinStack 类:
+ * <p>
+ * MinStack() 初始化堆栈对象。
+ * void push(int val) 将元素val推入堆栈。
+ * void pop() 删除堆栈顶部的元素。
+ * int top() 获取堆栈顶部的元素。
+ * int getMin() 获取堆栈中的最小元素。
+ */
+public class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        minStack.push(Math.min(minStack.peek(), val));
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+}

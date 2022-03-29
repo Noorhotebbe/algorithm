cf39539936a7211ebdebb94b9ddf0e5840ba3e26
Alice <1428235589@qq.com>
2022-03-29 11:30:01 +0800

    无重复的最长子串

--- src/com/algorithm/string/stringlength/LongSubString.java
+++ src/com/algorithm/string/stringlength/LongSubString.java
@@ -1,4 +1,4 @@
-package com.algorithm.stringlength;
+package com.algorithm.string.stringlength;
 
 import java.util.HashMap;
 
@@ -6,9 +6,8 @@ import java.util.HashMap;
  * @Version: 1.0
  * @Author: alice
  * @Date: 2022-03-04 09:19
- * @Description:
- *      无重复的最长子串：
- *          给定一个字符串s ，请你找出其中不含有重复字符的最长子串的长度。
+ * @Description: 无重复的最长子串：
+ * 给定一个字符串s ，请你找出其中不含有重复字符的最长子串的长度。
  */
 public class LongSubString {
 
@@ -19,19 +18,19 @@ public class LongSubString {
         }
 
         //max是不重复子串的长度，left是元素序列比较是否重复的起始下标
-        int maxlength = 0,left = 0;
+        int maxlength = 0, left = 0;
         HashMap<Character, Integer> map = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
             //每次判断map中是否有重复元素，若有，遍历下标则后移一位
-            if (map.containsKey(s.charAt(i))){
-                left = Math.max(left,map.get(s.charAt(i))+1);
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
             }
 
             //不管是否map中有重复元素，每次都会更新元素的索引位置。
-            map.put(s.charAt(i),i);
+            map.put(s.charAt(i), i);
             //i-left+1:指的是当前下标减去起始下标，+1是因为是从下标0开始的，最终比较得到最长的长度
-            maxlength = Math.max(maxlength,i-left+1);
+            maxlength = Math.max(maxlength, i - left + 1);
         }
         return maxlength;
 

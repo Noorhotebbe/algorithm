61225ec8e22d9d43ebc8cf7254953570dffe92d0
yangzhiwei <alice711@yeah.net>
2022-05-26 15:25:21 +0800

    1

--- src/com/algorithm/array/subarrays/MaxSubArray.java
+++ src/com/algorithm/array/subarrays/MaxSubArray.java
@@ -7,7 +7,7 @@ package com.algorithm.array.subarrays;
  * @Description:
  *      最大子数组之和：
  *          给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），
- *          返回其最大和。子数组 是数组中的一个连续部分。
+ *          返回其最大和。子数组 是数组中的一个连续部分，。
  */
 public class MaxSubArray {
     public int maxSubArray(int[] nums){
